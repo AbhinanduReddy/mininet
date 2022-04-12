894bd71e26c33ffb464dd6078930bfbf8a303507
K Abhinandu Reddy <nanduc06@gmail.com>
2022-04-12 11:53:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -16,9 +16,20 @@ class MyTopo( Topo ):
     def build( self ):
         "Create custom topo."
         d={}
-        for i in range(250,300):
-            s='s'+str(i)
+#         for i in range(250,300):
+#             s='s'+str(i)
+#             d[s]=self.addSwitch(s)
+        with open(r'test_file.txt') as f:
+          line = f.readline()
+          print(line[0:3])
+          while line:
+            line = f.readline()
+            s='s'+str(line[0:3])
             d[s]=self.addSwitch(s)
+            s='s'+str(line[0:3])
+            x='s'+str(line[4::])
+            d[x.split('\n')[0]]=self.addSwitch(x.split('\n')[0])
+            self.addLink(d['s'+str(line[0:3])],d[x.split('\n')[0]])
         with open(r'test_file.txt') as f:
           line = f.readline()
           print(line[0:3])
