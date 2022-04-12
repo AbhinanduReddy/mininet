15f9cbe4575a6f2e64daadb2b33fb3dda173d4e7
K Abhinandu Reddy <nanduc06@gmail.com>
2022-04-12 12:18:50 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,64 @@
+"""Custom topology example
+
+Two directly connected switches plus a host for each switch:
+
+   host --- switch --- switch --- host
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def build( self ):
+        "Create custom topo."
+        d={}
+        file=open(r'test_file.txt')
+        lst=[]
+        for line in file:
+          lst.append([ float(x) for x in line.split()])
+          column1 = [ x[0] for x in lst]
+          column2 = [ x[1] for x in lst]
+        for i in column1:
+            s='s'+str(i)
+            d[s]=self.addSwitch(s)
+        for i in column2:
+          
+            s='s'+str(i)
+            d[s]=self.addSwitch(s)
+        for i in range(len(column1)):
+          self.addLink('s'+str(column1[i]),'s'+str(column2[i]))
+          
+#         with open(r'test_file.txt') as f:
+#           line = f.readline()
+#           print(line[0:3])
+#           while line:
+#             line = f.readline()
+# #             s='s'+str(line[0:3])
+# #             d[s]=self.addSwitch(s)
+# #             s='s'+str(line[0:3])
+#             x='s'+str(line[4::])
+#             d[x.split('\n')[0]]=self.addSwitch(x.split('\n')[0])
+#         with open(r'test_file.txt') as f:
+#           line = f.readline()
+#           print(line[0:3])
+#           while line:
+#             line = f.readline()
+#             x='s'+str(line[4::])
+#             self.addLink(d['s'+str(line[0:3])],d[x.split('\n')[0]])
+#          # Add hosts and switches
+#         leftHost = self.addHost( 'h1' )
+#         rightHost = self.addHost( 'h2' )
+#         leftSwitch = self.addSwitch( 's251' )
+#         rightSwitch = self.addSwitch( 's256' )
+
+#         # Add links
+#         self.addLink( leftHost, leftSwitch )
+#         self.addLink( leftSwitch, rightSwitch )
+#         self.addLink( rightSwitch, rightHost )
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
