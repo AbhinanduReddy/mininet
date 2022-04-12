a5ebbff8bb7b9668e4d0f516a3a7f085c471109f
K Abhinandu Reddy <nanduc06@gmail.com>
2022-04-12 11:15:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,73 +1,35 @@
-#!/usr/bin/python
+"""Custom topology example
 
-"""
-linuxrouter.py: Example network with Linux IP router
-This example converts a Node into a router using IP forwarding
-already built into Linux.
-The example topology creates a router and three IP subnets:
-    - 192.168.1.0/24 (r0-eth1, IP: 192.168.1.1)
-    - 172.16.0.0/12 (r0-eth2, IP: 172.16.0.1)
-    - 10.0.0.0/8 (r0-eth3, IP: 10.0.0.1)
-Each subnet consists of a single host connected to
-a single switch:
-    r0-eth1 - s1-eth1 - h1-eth0 (IP: 192.168.1.100)
-    r0-eth2 - s2-eth1 - h2-eth0 (IP: 172.16.0.100)
-    r0-eth3 - s3-eth1 - h3-eth0 (IP: 10.0.0.100)
-The example relies on default routing entries that are
-automatically created for each router interface, as well
-as 'defaultRoute' parameters for the host interfaces.
-Additional routes may be added to the router or hosts by
-executing 'ip route' or 'route' commands on the router or hosts.
+Two directly connected switches plus a host for each switch:
+
+   host --- switch --- switch --- host
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
 """
 
 from mininet.topo import Topo
-from mininet.net import Mininet
-from mininet.node import Node
-from mininet.log import setLogLevel, info
-from mininet.cli import CLI
 
-class LinuxRouter( Node ):
-    "A Node with IP forwarding enabled."
+class MyTopo( Topo ):
+    "Simple topology example."
 
-    def config( self, **params ):
-        super( LinuxRouter, self).config( **params )
-        # Enable forwarding on the router
-        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
-
-    def terminate( self ):
-        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
-        super( LinuxRouter, self ).terminate()
-
-
-class NetworkTopo( Topo ):
-    "A LinuxRouter connecting three IP subnets"
-
-    def build( self, **_opts ):
-        a={}
-        
+    def build( self ):
+        "Create custom topo."
+        d={}
         for i in range(250,351):
-            s='s'+str(id)
-            a[s]=self.addSwitch(s)
-        
-            
-        
-#         for i in range(250,351):
-#             r='r'+str(id)
-#             ipp='10.'+str(id)+'.4.14'
-#             self.addNode(r,cls=LinuxRouter,ip=ipp)
-        
+            s='s'+str(i)
+            d[s]=self.addSwitch(s)
+
+        # Add hosts and switches
+        leftHost = self.addHost( 'h1' )
+        rightHost = self.addHost( 'h2' )
+        leftSwitch = self.addSwitch( 's251' )
+        rightSwitch = self.addSwitch( 's256' )
+
+        # Add links
+        self.addLink( leftHost, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( rightSwitch, rightHost )
 
 
-def run():
-    "Test linux router"
-    topo = NetworkTopo()
-    net = Mininet( topo=topo )  # controller is used by s1-s3
-    net.start()
-    info( '*** Routing Table on Router:\n' )
-    print net[ 'r0' ].cmd( 'route' )
-    CLI( net )
-    net.stop()
-
-if __name__ == '__main__':
-    setLogLevel( 'info' )
-    run()
+topos = { 'mytopo': ( lambda: MyTopo() ) }
